75793ef0ccec4be0ce00b40a29d6b20fc0774e3c
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2024-01-28 21:07:08 +0900

    session3 end

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,19 +2,39 @@ package hello.core;
 
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//Spring에서는 설정정보(구성정보)에 @Configuration을 적어주게 되어있음
+@Configuration
 public class AppConfig {
 
+    //@Bean을 적어주면 해당 메서드들이 Spring Container에 등록됨.
+    @Bean
     //생성자를 통해서 객체가 생성된게 들어가서 생성자 주입이라고 함.
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     //이렇게 만들어서 테스트 하는 것은 당연히 비효율적이고 메모리에 남으니 jUnit으로 테스트 할 것.
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+/*        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();*/
+
+        //ApplicationContext가 spring container라고 생각하면됨. 그래서 스프링은 대부분 이걸로 시작
+        /*AnnotationConfigApplicationContext: 우리가 config파일에서 빈 등록은 annotation으로 했기 때문에 사용.
+                                              => 그래서 파라미터로 AppConfig 클래스를 주입 */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig에서 memberService이름으로 등록된 빈을 꺼내는 코드
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         //MemberService service = new MemberServiceImpl();
         Member member = new Member(1L, "Hyeonung", Grade.VIP);
         memberService.join(member);
