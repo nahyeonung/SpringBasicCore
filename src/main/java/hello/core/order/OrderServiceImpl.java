3074349e673cabe43964a5bfa43f0b4f165e62af
nahyeonung <ntweety@naver.com>
2024-03-07 17:33:03 +0900

    lombok 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //필수 값인 final이 붙은 필드를 가지고 생성자를 만들어줌! 개꿀!! 필드 주입보다 더 깔끔해짐
 public class OrderServiceImpl implements OrderService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -48,6 +50,8 @@ public class OrderServiceImpl implements OrderService {
         private final MemberRepository memberRepository;
         private final DiscountPolicy discountPolicy;
 
+
+        /* 이제 롬복의 @RequiredArgsConstructor 덕분에 아래 코드를 자동으로 생성해줌
         //스프링도 어쨌든 자바고 생성자를 호출하려면 객체를 생성해야 하기 때문에,
         //컴포넌트 스캔시 new OrderServiceImpl(memberRepository, discountPolicy)를 자동으로 해서 의존성 주입을 시켜준다고 보면 된다.
         @Autowired //의존성 자동 주입을 위한 어노테이션 ex)ac.getBean(), 생성자가 하나일 때 생략해도 무방하다!!
@@ -57,6 +61,7 @@ public class OrderServiceImpl implements OrderService {
             this.memberRepository = memberRepository;
             this.discountPolicy = discountPolicy;
         }
+        */
     /**/
 
     @Override
